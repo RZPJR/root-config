257f5193871dc519e59fb8d6b73cffffcb526d75
junior <junior@edenfarm.id>
2023-02-06 14:53:53 +0700

    update routes

--- src/vue-mf-root-config.js
+++ src/vue-mf-root-config.js
@@ -1,22 +1,6 @@
 import { registerApplication, start } from "single-spa";
 import _ from "lodash";
-
-// untuk init route & app name
-const routes = {
-  "/": "@vue-mf/navbar",
-  "/home": "@vue-mf/home",
-  "/auth": "@vue-mf/auth",
-  "/error": "@vue-mf/error-handling",
-  "/user": "@vue-mf/user",
-  "/catalog": "@vue-mf/catalog",
-  "/configuration": "@vue-mf/configuration",
-  "/campaign": "@vue-mf/campaign",
-  "/customer-relation": "@vue-mf/crm",
-  "/logistic": "@vue-mf/logistic",
-  "/finance": "@vue-mf/finance",
-  "/purchasing": "@vue-mf/purchasing",
-  "/site": "@vue-mf/site"
-};
+import routes from "./routes"
 
 // untuk load route & app name
 const loadMicrofrontend = async (name) => {
@@ -28,7 +12,6 @@ const loadMicrofrontend = async (name) => {
     return Promise.reject(err);
   }
 }
-
 
 // looping route & app name untuk di register
 Object.entries(routes).forEach(([route, name]) => {

865daf1273415e1bec2a6784e78a418706c27097
junior <junior@edenfarm.id>
2023-02-17 16:01:45 +0700

    update webpack to build favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = (env) => ({
   entry: path.resolve(__dirname, "src/vue-mf-root-config"),
@@ -36,6 +37,9 @@ module.exports = (env) => ({
       },
     }),
     new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "public", to: "." }],
+    }),
   ],
   externals: ["single-spa", "vue", "vue-router", /^@vue-mf\/.+$/],
 });
